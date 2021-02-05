cfb5ec1d304c6edfc2832166377546b9b2f8bd7b
caolong <2397776038@qq.com>
2021-02-05 13:38:18 +0800

    jvm 字节码

--- src/cn/cal/javase/jvm/ByteCodeTest.java
+++ src/cn/cal/javase/jvm/ByteCodeTest.java
@@ -2,8 +2,43 @@ package cn.cal.javase.jvm;
 
 public class ByteCodeTest {
     public void testAdd() {
-        byte i = 15;
-        int j = 8;
+        byte i = 5;
+        int j = 3;
         int k = i + j;
+    }
+
+    public static void a() {
+        int i = 0;
+        int j = i++;
+        System.out.println(j);
+    }
+
+    public static void b() {
+        int i = 0;
+        int j = ++i;
+        System.out.println(j);
+    }
+
+    public static void c() {
+        int i = 0;
+        i = i++;
+        System.out.println(i);
+    }
+
+    public static void d() {
+        int i = 0;
+        i = ++i;
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        a();
+
+        b();
+
+        c();
+
+        d();
+
     }
 }
