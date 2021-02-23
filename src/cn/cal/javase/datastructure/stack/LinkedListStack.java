515821e04057c608d5531931e26506d2f1d8f6d9
caolong <2397776038@qq.com>
2021-02-23 15:33:33 +0800

    链式栈

--- src/cn/cal/javase/datastructure/stack/LinkedListStack.java
+++ src/cn/cal/javase/datastructure/stack/LinkedListStack.java
@@ -3,52 +3,51 @@ package cn.cal.javase.datastructure.stack;
 import cn.cal.javase.datastructure.linked.LinkedList;
 
 /**
- * 
  * 描述：基于链表自己实现的栈
- * 
+ *
  * @author 曹启龙
  * @date 2019-03-27 15:33
  */
 public class LinkedListStack<E> implements Stack<E> {
 
-	private LinkedList<E> list;
+    private LinkedList<E> list;
 
-	public LinkedListStack() {
-		list = new LinkedList<>();
-	}
+    public LinkedListStack() {
+        list = new LinkedList<>();
+    }
 
-	@Override
-	public int getSize() {
-		return list.getSize();
-	}
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return list.isEmpty();
-	}
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
 
-	// 压栈
-	@Override
-	public void push(E e) {
-		list.addFirst(e);
-	}
+    // 压栈
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
 
-	// 出栈
-	@Override
-	public E pop() {
-		return list.removeFirst();
-	}
+    // 出栈
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
 
-	@Override
-	public E peek() {
-		return list.getFirst();
-	}
+    @Override
+    public E peek() {
+        return list.getFirst();
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder res = new StringBuilder();
-		res.append("Stack: top ");
-		res.append(list);
-		return res.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: top ");
+        res.append(list);
+        return res.toString();
+    }
 }
